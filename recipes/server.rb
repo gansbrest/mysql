2f15890bafbffd4e7c20507761ac6d72381e51bb
Sergey Khaladzinski <gansbrest@yahoo.com>
2013-08-05 22:21:01 -0400

    Allow my.cnf overrides before mysql is started for the firs time, especially important for changes like max_binlog_size where you can't just restart mysql after the cange

--- recipes/server.rb
+++ recipes/server.rb
@@ -93,8 +93,29 @@ end
 node['mysql']['server']['packages'].each do |package_name|
   package package_name do
     action :install
-    notifies :start, "service[mysql]", :immediately
   end
+end
+
+unless platform_family?(%w{mac_os_x})
+  template "#{node['mysql']['conf_dir']}/my.cnf" do
+    source "my.cnf.erb"
+    owner "root" unless platform? 'windows'
+    group node['mysql']['root_group'] unless platform? 'windows'
+    mode "0644"
+    case node['mysql']['reload_action']
+    when 'restart'
+      notifies :restart, "service[mysql]", :immediately
+    when 'reload'
+      notifies :reload, "service[mysql]", :immediately
+    else
+      Chef::Log.info "my.cnf updated but mysql.reload_action is #{node['mysql']['reload_action']}. No action taken."
+    end
+    variables :skip_federated => skip_federated
+  end
+end
+
+service "mysql" do
+  action :start
 end
 
 unless platform_family?(%w{mac_os_x})
@@ -199,23 +220,4 @@ unless platform_family?(%w{mac_os_x})
     end
   end
 
-  template "#{node['mysql']['conf_dir']}/my.cnf" do
-    source "my.cnf.erb"
-    owner "root" unless platform? 'windows'
-    group node['mysql']['root_group'] unless platform? 'windows'
-    mode "0644"
-    case node['mysql']['reload_action']
-    when 'restart'
-      notifies :restart, "service[mysql]", :immediately
-    when 'reload'
-      notifies :reload, "service[mysql]", :immediately
-    else
-      Chef::Log.info "my.cnf updated but mysql.reload_action is #{node['mysql']['reload_action']}. No action taken."
-    end
-    variables :skip_federated => skip_federated
-  end
-
-  service "mysql" do
-    action :start
-  end
 end
